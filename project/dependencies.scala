97dab4a036ce7c66f86761f48f415a23005c8934
Max Streese <max.streese@smarttra.de>
2022-12-05 22:35:03 +0100

    Implement code to set up a given day

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -10,13 +10,15 @@ object dependencies {
   object versions {
     val osLib     = "0.8.1"
     val scalatest = "3.2.14"
+    val scopt     = "4.1.0"
   }
 
   object libs {
-    val osLib     = "com.lihaoyi"   %% "os-lib"    % versions.osLib
-    val scalatest = "org.scalatest" %% "scalatest" % versions.scalatest
+    val osLib     = "com.lihaoyi"      %% "os-lib"    % versions.osLib
+    val scalatest = "org.scalatest"    %% "scalatest" % versions.scalatest
+    val scopt     = "com.github.scopt" %% "scopt"     % versions.scopt
   }
 
-  val allDeps: Seq[ModuleID] = testScope(libs.osLib, libs.scalatest)
+  val allDeps: Seq[ModuleID] = compileScope(libs.osLib, libs.scopt) ++ testScope(libs.scalatest)
 
 }
