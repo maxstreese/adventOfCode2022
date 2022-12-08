f5d90bfc4d34c2f05c40d0c8fb317717bce769c1
Max Streese <max.streese@smarttra.de>
2022-12-08 22:23:28 +0100

    Fail at implementing something so far

--- src/test/scala/com/streese/adventofcode2020/PuzzlesSpec.scala
+++ src/test/scala/com/streese/adventofcode2020/PuzzlesSpec.scala
@@ -5,6 +5,10 @@ import org.scalatest.matchers.should.Matchers
 
 abstract class PuzzlesSpec(day: Int) extends AnyFlatSpec with Matchers {
 
-  val lines: Seq[String] = os.read.lines(os.resource / f"day$day%02.0f" / "input.txt")
+  lazy val lines: Seq[String] =
+    os.read.lines(os.resource / f"day$day%02.0f" / "input.txt")
+
+  lazy val example: Seq[String] =
+    os.read.lines(os.resource / f"day$day%02.0f" / "example.txt")
 
 }
