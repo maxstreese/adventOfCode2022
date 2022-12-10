9387f18d7168b0f292b8ddb4ee14e4c4ada413ff
Max Streese <max.streese@smarttra.de>
2022-12-10 12:35:30 +0100

    Set up day 10

---
+++ src/main/scala/com/streese/aoc/day10/puzzles.scala
@@ -0,0 +1,5 @@
+package com.streese.aoc.day10
+
+def part01(lines: Seq[String]): Unit = ???
+
+def part02(lines: Seq[String]): Unit = ???
---
+++ src/test/scala/com/streese/aoc/day10/Day10Spec.scala
@@ -0,0 +1,17 @@
+package com.streese.aoc.day10
+
+import com.streese.aoc.PuzzlesSpec
+
+class Day10Spec extends PuzzlesSpec(10) {
+
+  "part01" should "be correct" in {
+    // println(part01(lines))
+    // part01(lines) shouldBe ???
+  }
+
+  "part02" should "be correct" in {
+    // println(part02(lines))
+    // part02(lines) shouldBe ???
+  }
+
+}
