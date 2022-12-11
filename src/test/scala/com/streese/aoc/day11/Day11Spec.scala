04d46bdd4e29caab49a7086c8b097aeb7bd24a60
Max Streese <max.streese@smarttra.de>
2022-12-11 13:23:23 +0100

    Set up day 11; Update the description of day 10

---
+++ src/test/scala/com/streese/aoc/day11/Day11Spec.scala
@@ -0,0 +1,17 @@
+package com.streese.aoc.day11
+
+import com.streese.aoc.PuzzlesSpec
+
+class Day11Spec extends PuzzlesSpec(11) {
+
+  "part01" should "be correct" in {
+    // println(part01(input))
+    // part01(input) shouldBe ???
+  }
+
+  "part02" should "be correct" in {
+    // println(part02(input))
+    // part02(input) shouldBe ???
+  }
+
+}
