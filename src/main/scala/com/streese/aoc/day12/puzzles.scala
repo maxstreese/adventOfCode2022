a5c36881dbd78a6aba34664c6ba9d6ca5f1a6907
Max Streese <max.streese@smarttra.de>
2022-12-12 12:37:08 +0100

    set stuff up

---
+++ src/main/scala/com/streese/aoc/day12/puzzles.scala
@@ -0,0 +1,5 @@
+package com.streese.aoc.day12
+
+def part01(input: Seq[String]) = ???
+
+def part02(input: Seq[String]) = ???
---
+++ src/test/scala/com/streese/aoc/day12/Day12Spec.scala
@@ -0,0 +1,17 @@
+package com.streese.aoc.day12
+
+import com.streese.aoc.PuzzlesSpec
+
+class Day12Spec extends PuzzlesSpec(12) {
+
+  "part01" should "be correct" in {
+    // println(part01(input))
+    // part01(input) shouldBe ???
+  }
+
+  "part02" should "be correct" in {
+    // println(part02(input))
+    // part02(input) shouldBe ???
+  }
+
+}
