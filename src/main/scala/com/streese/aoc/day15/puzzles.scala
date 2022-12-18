d3e78cc4bf6e2d5ab2ed698d07b440a25141bc0c
Max Streese <max.streese@smarttra.de>
2022-12-18 15:57:21 +0100

    Solve day 15 part 2

--- src/main/scala/com/streese/aoc/day15/puzzles.scala
+++ src/main/scala/com/streese/aoc/day15/puzzles.scala
@@ -51,14 +51,31 @@ def withinManhattenInY(y: Int, sensors: Seq[Sensor]): Seq[Interval] =
             case Some(combined) => combined :: t
         case Nil => List(a)
     }
+    .reverse
 
 def beaconsInY(y: Int, sensors: Seq[Sensor]): Set[Int] = sensors.map(_.b).filter(_.y == y).map(_.x).toSet
 
-def solvePart01(y: Int, sensors: Seq[Sensor]) =
+def solvePart01(y: Int, sensors: Seq[Sensor]): Int =
   val taken   = withinManhattenInY(y, sensors)
   val beacons = beaconsInY(y, sensors).filter(b => taken.exists(_.contains(b))).size
   taken.foldLeft(0)((acc, a) => acc + a.taken) - beacons
 
+def availableInY(y: Int, from: Int, to: Int, sensors: Seq[Sensor]): Option[Int] =
+  val taken   = withinManhattenInY(y, sensors)
+  val left    = taken.headOption.map(_.from).filter(_ > from).map(to => Interval(from, to)).toSeq
+  val right   = taken.lastOption.map(_.to).filter(_ < to).map(from => Interval(from, to)).toSeq
+  val between = taken.zip(taken.tail).map((l, r) => Interval(l.to + 1, r.from - 1))
+  if taken.isEmpty then Some(from)
+  else (left ++ between ++ right).headOption.map(_.from)
+
+def solvePart02(maxX: Int, maxY: Int, sensors: Seq[Sensor]): Long =
+  var beacon: Option[Point] = None
+  var y = 0
+  while beacon.isEmpty && y <= maxY do
+    beacon = availableInY(y, 0, maxX, sensors).map(Point(_, y))
+    y += 1
+  beacon.map(b => b.x * 4000000L + b.y).getOrElse(0L)
+
 def part01(input: Seq[String]): Int = solvePart01(2000000, parse(input))
 
-def part02(input: Seq[String]) = ???
+def part02(input: Seq[String]) = solvePart02(4000000, 4000000, parse(input))
--- src/test/scala/com/streese/aoc/day15/Day15Spec.scala
+++ src/test/scala/com/streese/aoc/day15/Day15Spec.scala
@@ -6,18 +6,13 @@ import scala.util.chaining._
 
 class Day15Spec extends PuzzlesSpec(15) {
 
-  "" should "" in {
-    // val res = parse(input).pipe(solvePart01(2000000, _))
-    // pprint.pprintln(res)
-  }
 
   "part01" should "be correct" in {
     part01(input) shouldBe 5508234
   }
 
   "part02" should "be correct" in {
-    // println(part02(input))
-    // part02(input) shouldBe ???
+    part02(input) shouldBe 10457634860779L
   }
 
 }
